f99090e3139b5c13f8e44d47ce5a6b9bb13943b2
George Crisan <georgerdp@gmail.com>
2020-05-15 12:37:55 +0100

    feat: style log in component

--- Views/Login.js
+++ Views/Login.js
@@ -18,6 +18,9 @@ import {
   StatusBar,
 } from 'react-native';
 
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
 // Style
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
 
@@ -31,12 +34,15 @@ const styles = StyleSheet.create({
   gradient: {
     flex: 1
   },
-  form: {
+  formCard: {
     flex: 1,
-    height: 500,
+    backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16
+    maxHeight: 400,
+    padding: 16,
+    marginLeft: 16,
+    borderRadius: 5,
+    marginRight: 16
   },
   sectionContainer: {
     marginTop: 32,
@@ -63,6 +69,10 @@ const styles = StyleSheet.create({
   statusBar: {
     height: STATUSBAR_HEIGHT,
   },
+  input: {
+    padding: 32,
+    marginBottom: 16
+  }
 });
 
 
@@ -72,9 +82,20 @@ const Login = (props) => {
   useEffect(() => {
     //props.authEmailPass('georgerdp@gmail.com','test22');
   }, []);
+   
+  const [borderBottomColor, setBorderBottomColor] = useState({
+    unu: 'gray',
+    doi: 'gray'
+  });
 
   const headerHeight = useHeaderHeight();
 
+  const setBorderBottomColorT = (type,color)=> {
+      let borderBottomColor2 = {...borderBottomColor};
+      borderBottomColor2[type] = color;
+      console.log(borderBottomColor2, 'now');
+      setBorderBottomColor(borderBottomColor2);
+  }
 
   return (
     <View style={{ flex: 1, marginTop: headerHeight }}>
@@ -83,11 +104,61 @@ const Login = (props) => {
         start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
         colors={['#242478', '#3b5998', '#1b9bb5']}>
         <Header customText={'Please log in to your account'} />
-        <View style={styles.form}>
-          <Text > !!!!!!!!!!!!!!!!!! </Text>
-          <Text > !!!!!!!!!!!!!!!!!! </Text>
-          <Text > !!!!!!!!!!!!!!!!!! </Text>
+
+        <View style={styles.formCard}>
+          <Icon
+            name="user-circle"
+            size={50}
+            color="#faa500"
+            style={{ alignSelf: 'center', paddingBottom: 16 }}
+          />
+          <Input
+            placeholder="Email"
+            label='Email Address'
+            onFocus={()=>{ setBorderBottomColorT('unu', '#2196f3') }}
+            onBlur={()=>{ setBorderBottomColorT( 'unu', 'grey') }}
+            inputContainerStyle={{
+              borderBottomColor: borderBottomColor['unu']
+            }}
+            leftIcon={
+              <Icon
+                name='at'
+                size={24}
+                color={borderBottomColor['unu'] ?? 'gray'}
+                style={{marginRight: 4}}
+              />
+            }
+            style={styles.input}
+            onChangeText={() => { }}
+          />
+          <Input
+            placeholder="Password"
+            label='Password'
+            onFocus={()=>{ setBorderBottomColorT('doi','#2196f3') }}
+            onBlur={()=>{ setBorderBottomColorT('doi', 'gray') }}
+            inputContainerStyle={{
+              borderBottomColor: borderBottomColor['doi']
+            }}
+            leftIcon={
+              <Icon
+                name='lock'
+                size={24}
+                style={{marginRight: 4}}
+                color={borderBottomColor['doi'] ?? 'gray'}
+              />
+            }
+            style={styles.input}
+            onChangeText={() => { }}
+          />
+          <Button
+            title="Log in"
+            type="outline"
+            buttonStyle={{borderWidth: 1.6}}
+            titleStyle={{fontFamily: 'Mina-Regular', fontWeight: 'bold'}}
+            style={{marginTop: 16}}
+          />
         </View>
+
       </LinearGradient>
     </View>
   );
