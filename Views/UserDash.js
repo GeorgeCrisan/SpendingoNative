9dc1ad0c0ab47106d3e6386d1399be1b890a73de
George Crisan <georgerdp@gmail.com>
2020-05-19 02:07:48 +0100

    chore: do some bits

--- Views/UserDash.js
+++ Views/UserDash.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { loginUser, logoutUser, fetchBudgets } from '../actions';
+import { connect } from 'react-redux';
 
 import {
   SafeAreaView,
@@ -13,24 +15,67 @@ import {
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   backgroundColor: 'blue'
-  } 
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  formCard: {
+    flex: 1,
+    backgroundColor: '#fff',
+    maxHeight: '70%',
+    padding: 16,
+    marginLeft: 16,
+    borderRadius: 5,
+    marginTop: 10,
+    marginRight: 16
+  },
 });
 
 import LinearGradient from 'react-native-linear-gradient';
 
-const UserDash = ({navigation}) => {
+const UserDash = (props, { navigation }) => {
+
+  const { loading, budgets } = props;
+  useEffect(() => {
+    props.fetchBudgets();
+  }, []);
+
+  console.log(budgets, 'man');
+
   return (
     <SafeAreaView style={styles.container}>
       <LinearGradient
-        style={{flex: 1}}
+        style={{ flex: 1 , justifyContent: 'flex-start'}}
         start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
         colors={['#242478', '#3b5998', '#1b9bb5']}>
-      <Text style={{flex: 1,color: 'black', fontSize: 100}} > Hello world 1 </Text>
+        
+        <Text style={{ color: 'white', fontSize: 26, textAlign: 'center', marginBottom: 16 }} > Selected Budget </Text>
+        <View style={{...styles.formCard}}>
+          {loading && <ActivityIndicator size="large" color="#242478" />}
+        </View>
       </LinearGradient>
     </SafeAreaView>
   );
 };
 
-export default UserDash;
+function mapDsipatchToProps(dispatch) {
+  return {
+    logOut: () => {
+      dispatch(logoutUser());
+    },
+    fetchBudgets: () => {
+      dispatch(fetchBudgets());
+    },
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.auth.user,
+    isAuthenticated: state.auth.isAuthenticated,
+    loading: state.budgets.loading,
+    budgets: state.budgets.budgets
+
+  };
+}
+
+export default connect(mapStateToProps, mapDsipatchToProps)(UserDash);
